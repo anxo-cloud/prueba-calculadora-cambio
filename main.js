91a8fc9892d00b760a57e7bae3c7e70d09d519c4
anxo-cloud <anxobreoganFuentes@gmail.com>
2019-11-13 11:13:53 +0100

    Prueba de fetch en main

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ function leer (){
     output.innerText=resultado;
 }
 launchButton.addEventListener('click',leer);
+
+//Prueba de fetch!!!!
+let url='https://pokeapi.co/api/v2/pokemon/4'
+fetch(url)
+    .then(res=> res.json())
+    .then (data=> {
+        output.innerText=data.id;
+        console.log(data);
+    })
+
+
+    
